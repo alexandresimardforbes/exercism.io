f5ed87b94814a5f8cba6fe29b6f500d463e36b7a
Alexande Simard Forbes <alexandresimardforbes@gmail.com>
2020-10-02 17:57:13 -0400

    Summit solution for Transcription exercise

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -3,6 +3,31 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRna = () => {
-  throw new Error("Remove this statement and implement this function");
+export const toRna = (nucleotides) => {
+
+  let nucleotidesArray = [...nucleotides];
+  console.log(nucleotidesArray);
+
+  for (let i = 0; i < nucleotidesArray.length; i++) {
+    switch (nucleotidesArray[i]) {
+      case "G":
+        nucleotidesArray[i] = "C";
+        break;
+      case "C":
+        nucleotidesArray[i] = "G";
+        break;
+      case "T":
+        nucleotidesArray[i] = "A";
+        break;
+      case "A":
+        nucleotidesArray[i] = "U";
+        break;
+      default:
+        nucleotides = ""
+    }
+  }
+
+  nucleotides = nucleotidesArray.join("");
+  return nucleotides
+
 };
